5aa129f2a1b36b1cb745f677eacb7b7c7547bb58
Hanieh73 <119684905+Hanieh73@users.noreply.github.com>
2023-06-07 17:01:03 +0100

    changed view cv to the download function button

--- src/index.js
+++ src/index.js
@@ -4,3 +4,16 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+
+const express = require('express');
+const app = express();
+
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 1234');
+});
